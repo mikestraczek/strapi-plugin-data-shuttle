b65b407dd21c3ddf5a3df368fd507ffec2d46eb5
Mike Straczek <mike.straczek@next-levels.de>
2025-03-28 16:25:33 +0100

    feat: add Header, PluginIcon, Alerts, and InjectedExportCollectionType components
    - Introduced Header component for displaying plugin title and description.
    - Added PluginIcon component to represent the database visually.
    - Implemented Alerts component for managing and displaying alerts using a portal.
    - Created InjectedExportCollectionType component to encapsulate the ExportModal functionality.
    - Added useForm hook for managing form state and options.
    
    Time to sprinkle some cozy vibes into the plugin! ☕✨

---
+++ admin/src/components/Injected/Alerts/Alerts.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+
+import { Alert, Portal } from '@strapi/design-system';
+
+import { useAlerts } from '../../../hooks/useAlerts';
+
+export const Alerts = () => {
+  const { alerts, removeAlert } = useAlerts();
+
+  return (
+    <Portal>
+      <div className="plugin-ie-alerts">
+        {alerts?.map(({ id, title, message, variant }) => (
+          <Alert
+            key={id}
+            closeLabel="Close"
+            title={title}
+            variant={variant}
+            onClose={() => removeAlert(id)}
+          >
+            {message}
+          </Alert>
+        ))}
+      </div>
+    </Portal>
+  );
+};
